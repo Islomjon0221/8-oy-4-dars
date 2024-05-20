8184d8a3026b9508344627fe2bbd180c22c83919
Islomjon0221 <islomjon20110221@gmail.com>
2024-05-20 15:56:36 +0500

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -45,18 +45,18 @@ function App() {
           Why don't you take a challenge😏
         </h1>
         <div className="gap-14 flex items-center text-center">
-          <button className="text-red-600 font-bold transition-all hover:bg-red-50 p-2 rounded-lg">
-            Pomodoro <span className="font-extrabold text-red-600">0</span>
+          <button className="text-blue-600 font-bold transition-all hover:bg-red-50 p-2 rounded-lg">
+            Pomodoro <span className="font-extrabold text-blue-600">0</span>
           </button>
           <button className="text-green-700 font-bold transition-all hover:bg-green-50 p-2 rounded-lg">
             Rest <span className="font-extrabold text-green-700">0</span>
           </button>
-          <button className="text-blue-600 transition-all font-bold hover:bg-blue-50 p-2 rounded-lg">
-            Long Rest <span className="font-extrabold text-blue-700">0</span>
+          <button className="text-red-600 transition-all font-bold hover:bg-red-50 p-2 rounded-lg">
+            Long Rest <span className="font-extrabold text-red-700">0</span>
           </button>
         </div>
         <div className="mt-20">
-          <div className="m-3 flex h-72 w-72 items-center text-red-500 text-7xl justify-center rounded-full bg-red-50 border-4 border-red-200 ">
+          <div className="m-3 flex h-72 w-72 items-center text-blue-500 text-7xl justify-center rounded-full bg-blue-50 border-4 border-blue-200 hover:bg-blue-100">
             <span>{minutes}</span>
             <span>:</span>
             <span>{seconds}</span>
@@ -67,7 +67,7 @@ function App() {
           {!isRun && (
             <button
               onClick={startTimer}
-              className="inline-block px-8 py-2 text-lg font-medium text-white bg-red-600 rounded-lg transition-all hover:bg-red-700"
+              className="inline-block px-8 py-2 text-lg font-medium text-white bg-blue-600 rounded-lg transition-all hover:bg-blue-700"
             >
               START
             </button>
@@ -75,7 +75,7 @@ function App() {
           {isRun && (
             <button
               onClick={stopTimer}
-              className="inline-block px-8 py-2 text-lg font-medium text-white bg-red-600 rounded-lg transition-all hover:bg-red-700"
+              className="inline-block px-8 py-2 text-lg font-medium text-white bg-blue-600 rounded-lg transition-all hover:bg-blue-700"
             >
               STOP
             </button>
@@ -83,7 +83,7 @@ function App() {
 
           <button
             onClick={resetTimer}
-            className="inline-block px-8 py-2 text-lg font-medium text-white bg-red-600 rounded-lg transition-all hover:bg-red-700"
+            className="inline-block px-8 py-2 text-lg font-medium text-white bg-blue-600 rounded-lg transition-all hover:bg-blue-700"
           >
             RESET
           </button>
